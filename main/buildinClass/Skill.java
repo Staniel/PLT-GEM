8d124c0db704b6ca1872cd8ec3462af4b468ce09
Ted Li <tl2493@columbia.edu>
2015-05-01 16:48:39 -0400

    more game logics added.

--- main/buildinClass/Skill.java
+++ main/buildinClass/Skill.java
@@ -6,7 +6,7 @@ public class Skill {
 		public double defendMod;
 		public String name;
 		
-		public Skill(float hMod, float aMod, float dMod, String name) {
+		public Skill(String name, double hMod, double aMod, double dMod) {
 			this.lifeMod = hMod;
 			this.attackMod = aMod;
 			this.defendMod = dMod;
@@ -16,25 +16,29 @@ public class Skill {
 		public Hero cast(Hero h) {
 			Hero modHero = new Hero(h);
 			if (lifeMod > 0) {
-				System.out.printf("%s restored %.2f health\n", this.name, modHero.life);
+				System.out.printf("%s restored %.2f health for %s.\n", this.name, this.lifeMod, modHero.name);
 				modHero.life += lifeMod;
 			}
 			
-			if (attackMod > 1) {
-				modHero.attack *= attackMod;
+			if (attackMod > 0) {
+				System.out.println("Attack increased temporarily by " + attackMod * 100 + "% for " + modHero.name + ".");
+				modHero.attack *= (1 + attackMod);
 			}
 			
-			if (defendMod > 1) {
-				modHero.defend *= defendMod;
+			if (defendMod > 0) {
+				System.out.println("Defense increased temporarily by " + defendMod * 100 + "% for " + modHero.name + ".");
+				modHero.defend *= (1 + defendMod);
 			}
 			
 			return modHero;
 		}
 		
 		public String effect() {
-			String lifeEffect = (this.lifeMod > 0) ? "Heal hero for " + lifeMod + "life and" :"";
-			String attackEffect = (this.attackMod > 0) ? "Heal hero for " + lifeMod + "life and" :"";
-			String defendEffect = (this.defendMod > 0) ? "Heal hero for " + lifeMod + "life" :"";
+			String lifeEffect = (this.lifeMod > 0) ? "Heal caster for " + lifeMod + ". " :"";
+			String attackEffect = (this.attackMod > 0) ? "Increase attack by " + attackMod * 100
+					+ "% for this round. " :"";
+			String defendEffect = (this.defendMod > 0) ? "Increase defense by " + defendMod * 100
+					+ "% for this round." :"";
 			String totalEffect = lifeEffect + attackEffect + defendEffect;
 			return totalEffect;
 		}
--- test/buildinClass/BattleTest.java
+++ test/buildinClass/BattleTest.java
@@ -9,8 +9,11 @@ import buildinClass.Battle;
 public class BattleTest {
 	@Test
 	public void doTest(){
-		Monster m = new Monster("boss", 0.5, 0.5, 10, null);
-		Hero h = new Hero("hero", 1.0, 1.0, 5.0, null);
+		Skill s1 = new Skill("Empower", 1.0, 0.2, 0.2);
+		Skill s2 = new Skill("Heal", 5.0, 0, 0);
+		Skill[] ss = {s1, s2};
+		Monster m = new Monster("boss", 5, 2, 20, ss);
+		Hero h = new Hero("hero", 5, 2, 10, ss);
 		Battle b = new Battle("hey", m);
 		Assert.assertEquals(b.trigger(h), true);
 	}
