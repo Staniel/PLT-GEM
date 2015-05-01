b072fce67c4cc8e5eca165af9e69aa18acb2e164
Hong Guo <Edward@dyn-160-39-143-57.dyn.columbia.edu>
2015-05-01 17:33:50 -0400

    test battle

--- test/buildinClass/BattleTest.java
+++ test/buildinClass/BattleTest.java
@@ -9,6 +9,7 @@ import buildinClass.Battle;
 public class BattleTest {
 	@Test
 	public void doTest(){
+		// test the game when the hero is lost
 		Skill s1 = new Skill("Empower", 1.0, 0.2, 0.2);
 		Skill s2 = new Skill("Heal", 5.0, 0, 0);
 		Skill[] ss = {s1, s2};
@@ -16,5 +17,20 @@ public class BattleTest {
 		Hero h = new Hero("hero", 5, 2, 10, ss);
 		Battle b = new Battle("hey", m);
 		Assert.assertEquals(b.trigger(h), true);
+		
+		// test the display of the game
+		Assert.assertEquals(b.display, "hey");
+		
+	}
+	
+	@Test
+	public void testLost() {
+		// test lost
+		Skill s3 = new Skill("futile", 0, 0, 0);
+		Skill[] ss2 = {s3};
+		Hero h1 = new Hero("PLT", 5, 2, 0, ss2);
+		Monster m1 = new Monster("Aho", 5, 2, 20, ss2);
+		Battle b2 = new Battle("futile", m1);
+		Assert.assertEquals(b2.trigger(h1), false);
 	}
 }
