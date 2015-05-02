5b838f7aca025dc1bb76465cef02cf6220905198
Ted Li <tl2493@columbia.edu>
2015-05-02 01:47:26 -0400

    Fine tuning battle logic

--- main/buildinClass/Battle.java
+++ main/buildinClass/Battle.java
@@ -7,8 +7,9 @@ public class Battle {
 	private Scanner sc;
 	private Random rng;
 	private String display;
-	private Monster myBoss;
-	private String[] effects = {"badly effective", "poorly effective", "normally effective", "somewhat effective", "super effective"};
+	private Unit myBoss;
+	private Unit myHero;
+	private String[] effects = {"badly effective", "poorly effective", "somewhat effective", "very effective", "super effective"};
 	private double effectRNG;
 	private double bossDamage;
 	private double heroDamage;
@@ -17,7 +18,7 @@ public class Battle {
 	private int skillNum;
 	
 	//Initiate a battle with battle message and monster.
-	public Battle(String s, Monster m){
+	public Battle(String s, Unit m){
 		display = s;
 		myBoss = m;
 	}
@@ -25,39 +26,47 @@ public class Battle {
 	//Copy constructor.
 	public Battle(Battle b){
 		display = b.display;
-		myBoss = new Monster(b.myBoss);
+		myBoss = new Unit(b.myBoss);
 	}
 	
 	//Return true or false of victory or defeat.
-	public boolean trigger(Hero myHero){
+	public boolean trigger(Unit h){
 		System.out.printf("Encountered %s, ", myBoss.name);
 		System.out.println(this.display);
 		
 		sc = new Scanner(System.in);
 		rng = new Random();
+		this.myHero = h;
 		heroSkills = myHero.skills;
 		bossSkills = myBoss.skills;
 		
 		while (myBoss.life > 0 && myHero.life > 0){
 			System.out.println("Please choose your skill to use:");
 			System.out.println("0 - Attack: A \"normally\" effective attack.");
-			skillCheck(myHero, myBoss);
-			
+
 			//Use skill for hero, if any.
-			if (heroSkills != null && heroSkills.length > 0) {
-				myHero.showSkills();
-				skillNum = sc.nextInt() - 1;
-				System.out.printf("%s Used skill : %s\n", myHero.name, heroSkills[skillNum].name);
-				myHero = myHero.skills[skillNum].cast(myHero);
-				if (myHero.skill.lifeMod > 0) {
-					continue;
+			if (myHero.chi > 0 && heroSkills != null && heroSkills.length > 0) {
+				while (true) {
+					myHero.showSkills();
+					skillNum = sc.nextInt() - 1;
+					if (myHero.chi < heroSkills[skillNum].cost) {
+						System.out.println("You don't have enough chi for this skill");
+						continue;
+					}
 				}
+					
+				System.out.printf("%s Used skill : %s\n", myHero.name, heroSkills[skillNum].name);
+				myHero.skills[skillNum].cast(myHero);
 			}
 			
 			//Automatically use skill for boss, if any.
-			if (bossSkills != null && bossSkills.length > 0) {
+			if (myBoss.chi > 0 && bossSkills != null && bossSkills.length > 0) {
 				skillNum = rng.nextInt(bossSkills.length);
+				if (myBoss.chi < bossSkills[skillNum].cost) {
+					continue;
+				}
 				System.out.printf("%s Used skill : %s\n", myBoss.name, bossSkills[skillNum].name);
+				myBoss.skills[skillNum].cast(myBoss);
 				System.out.println("");
 			}
 			
@@ -65,7 +74,8 @@ public class Battle {
 			effectRNG =  generateRandom();
 			bossDamage = effectRNG * Math.max(0, myHero.attack - myBoss.defend);
 			if (bossDamage >= myBoss.life) {
-				System.out.printf("%s was defeated!\n", myBoss.name);
+				System.out.printf("%s defeated %s!\n", myHero.name, myBoss.name);
+				reward();
 				return true;
 			}
 			myBoss.life -= bossDamage;
@@ -85,17 +95,33 @@ public class Battle {
 					myBoss.name, myHero.name, heroDamage, effects[(int) (effectRNG / 0.25 - 2)]);
 			System.out.printf("%s has %.2f life left\n",myHero.name, myHero.life);
 			
-			//Anything else to handle before next round?
+			//Prepare for next round.
+			roundCheck();
 			System.out.println("");
 		}
 		
-		//Victory!
+		//How could you get here!?
 		return false;
 	}
 	
 	//Check duration of skill and reset status if expired.
-	private void skillCheck(Hero h, Monster m) {
+	private void roundCheck() {
+		myHero.duration--;
+		myBoss.duration--;
 		
+		if (myHero.duration <= 0)
+			myHero.skill.cancel(myHero);
+		if (myBoss.duration <= 0)
+			myBoss.skill.cancel(myBoss);
+		
+	}
+	
+	//You win the battle, you get stuff.
+	private void reward() {
+		this.myHero.attack *= 1.05;
+		this.myHero.defend *= 1.05;
+		this.myHero.lifeMax *= 1.05;
+		this.myHero.life = this.myHero.lifeMax;
 	}
 	
 	private double generateRandom() {
