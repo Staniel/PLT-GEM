7a433ec1f1513a8bd6479c4d86aa19ed4a3e6459
guohong630 <guohong_bs@hotmail.com>
2015-05-06 16:46:26 -0400

    add test individual file function

---
+++ Compiler/SystemTests/test1/unary.gem.java
@@ -0,0 +1,12 @@
+import java.util.*;public class Main {
+public static Scanner scanner = new Scanner(System.in);public static void main(String[] args){
+int i=0;
+i = i + 1;System.out.println(i);
+i = i - 2;System.out.println(i);
+i = i * 2;System.out.println(i);
+i = i / 2;System.out.println(i);
+i = i + -( 1 );System.out.println(i);
+System.out.println((i));
+}
+}
+
