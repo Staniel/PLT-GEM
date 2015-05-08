6166bcede4e15d7b6e3051e28419114e0c726ef1
Yuxuan Wang <yw2666@columbia.edu>
2015-05-08 15:01:06 -0400

    Add type checking visitor

---
+++ main/GEMTypeCheckVisitor.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
+public class GEMTypeCheckVisitor extends GEMBaseVisitor {
+	LinkedList<HashMap<String, VariableSymbol>> symbols = new LinkedList<HashMap<String, VariableSymbol>>();
+}
