e725c91880d3335549f4c4891b04209dd6bdd20a
terrence <lixiy1234@gmail.com>
2015-05-09 00:26:35 -0400

    fixed bug

--- main/GEMTypeCheckVisitor.java
+++ main/GEMTypeCheckVisitor.java
@@ -508,13 +508,14 @@ public class GEMTypeCheckVisitor extends GEMBaseVisitor <Object> {
 			ce(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine(), PARAS_MISMATCH, functionName);
 			return res;
 		}
-		System.err.print(functionParams.size());
 		for(int i=0;i<functionParams.size();i++){
-			if(functionParams.get(i).type != functionDefParams.get(i).type||functionParams.get(i).isFunction){
+			if(!functionParams.get(i).type.equals(functionDefParams.get(i).type)||functionParams.get(i).isFunction){
+				System.err.print(functionParams.get(i).type);
 				ce(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine(), PARAS_MISMATCH, functionName);
 				return res;
 			}
 			if(functionParams.get(i).arrayDimension != functionDefParams.get(i).arrayDimension){
+				System.err.print(2);
 				ce(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine(), PARAS_MISMATCH, functionName);
 				return res;
 			}
