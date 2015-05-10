2077575d4078006d63956cc887785f2a80b299ed
Ted Li <tl2493@columbia.edu>
2015-05-10 15:21:28 -0400

    buildin class code style check

--- main/buildinClass/Battle.java
+++ main/buildinClass/Battle.java
@@ -3,121 +3,143 @@ package buildinClass;
 import java.util.Scanner;
 
 public class Battle {
-	
-	//Information of this battle.
-	private Scanner sc;
-	private String display;
-	private Unit myBoss;
-	
-	public Unit myHero;
-	private int skillNum;
-	private int round;
-	
-	//Initiate a battle with battle message and monster.
-	public Battle(String s, Unit m){
-		display = s;
-		myBoss = m;
-	}
-	
-	//Copy constructor.
-	public Battle(Battle b){
-		this.display = b.display;
-		this.myBoss = b.myBoss;
-	}
-	
-	//Return true or false of victory or defeat.
-	public boolean trigger(Unit h){
-		System.out.printf("%s encountered %s, ", h.name, myBoss.name);
-		System.out.println(this.display);
-		
-		this.sc = new Scanner(System.in);
-		this.round = 0;
-		this.myHero = h;
-		
-		while (myBoss.life > 0 && myHero.life > 0){
-			myHero.status();
-			myBoss.status();
 
-			//Choose skill from user input.
-			if (myHero.skills != null && myHero.skills.length > 0) {
-				System.out.println("================================================================================");
-				System.out.println("Choose skill, or press enter to auto act:");
-				System.out.println("0 - Attack: A \"normally\" effective attack.");
-				myHero.showSkills();
-				while (true) {
-					String command = sc.nextLine();
-					if (command.isEmpty()) {
-						myHero.auto(myBoss);
-						break;
-					}
-					try {
-						skillNum = Integer.parseInt(command) - 1;
-						
-						//Chosen normal attack.
-						if (skillNum == -1)
-							break;
-						if (skillNum < -1 || skillNum >= myHero.skills.length) {
-							System.out.println("No such skill.");
-							continue;
-						}
-						if (myHero.chi < myHero.skills[skillNum].cost) {
-							System.out.println("You don't have enough chi for this skill.");
-							continue;
-						}
-						myHero.skills[skillNum].cast(myHero);
-						break;
-					} catch (NumberFormatException nfe) {
-						System.out.println("No such command.");
-						continue;
-					}
-				}
-			}
-			
-			//Automatically choose skill for boss, if any.
-			myBoss.auto(myHero);
-			
-			System.out.println("================================================================================");
-			//Hero round.
-			//Only attack if no life/chi skill is used.
-			myHero.attack(myBoss);
-			if (myBoss.life <= 0) {
-				roundOver();
-				reward();
-				return true;
-			}
-			
-			//Boss round.
-			//Only attack if no life/chi skill is used.
+  // Information of this battle.
+  private Scanner sc;
+  private String display;
+  private Unit myBoss;
 
-			myBoss.attack(myHero);
-			if (myHero.life <= 0) {
-				System.out.printf("%s was defeated!\n", myHero.name);
-				return false;
-			}
-			
-			//round clean up.
-			roundOver();
-		}
-		
-		//How could you get here!?
-		return false;
-	}
-	
-	//Check duration of skill and reset status if expired.
-	public void roundOver() {
-		if (this.myHero.skill != null)
-			this.myHero.skill.cancel(this.myHero);
-		if (this.myBoss.skill != null)
-			this.myBoss.skill.cancel(this.myBoss);
-		this.round++;
-		System.out.printf("round %d over\n\n\n\n", this.round);
-	}
-	
-	//You win the battle, you get good stuff and become stronger.
-	//May get stuff from monsters by adding logic here.
-	void reward() {
-		System.out.printf("%s defeated %s after %d %s !\n", myHero.name, myBoss.name, round, 
-				(round > 1) ? "rounds" : "round");
-		this.myHero.grow(this.myBoss);
-	}
+  public Unit myHero;
+  private int skillNum;
+  private int round;
+
+  /** 
+   * Initiate a battle with battle message and target.
+   * @param display display message of this battle.
+   * @param target target of this battle.
+   */
+  public Battle(String display, Unit target) {
+    this.display = display;
+    this.myBoss = target;
+  }
+
+  /**
+   *  Copy constructor.
+   * @param bat battle to copy from.
+   */
+  public Battle(Battle bat) {
+    this.display = bat.display;
+    this.myBoss = bat.myBoss;
+  }
+
+  /**
+   * Return true or false of victory or defeat.
+   * @param hero hero of this battle.
+   * @return True for win, False for defeat.
+   */
+  public boolean trigger(Unit hero) {
+    System.out.printf("%s encountered %s, ", hero.name, myBoss.name);
+    System.out.println(this.display);
+
+    this.sc = new Scanner(System.in);
+    this.round = 0;
+    this.myHero = hero;
+
+    while (myBoss.life > 0 && myHero.life > 0) {
+      myHero.status();
+      myBoss.status();
+
+      // Choose skill from user input.
+      if (myHero.skills != null && myHero.skills.length > 0) {
+        System.out
+            .println("====================================="
+                + "===========================================");
+        System.out.println("Choose skill, or press enter to auto act:");
+        System.out.println("0 - Attack: A \"normally\" effective attack.");
+        myHero.showSkills();
+        while (true) {
+          String command = sc.nextLine();
+          if (command.isEmpty()) {
+            myHero.auto(myBoss);
+            break;
+          }
+          try {
+            skillNum = Integer.parseInt(command) - 1;
+
+            // Chosen normal attack.
+            if (skillNum == -1) {
+              break;
+            }
+            if (skillNum < -1 || skillNum >= myHero.skills.length) {
+              System.out.println("No such skill.");
+              continue;
+            }
+            if (myHero.chi < myHero.skills[skillNum].cost) {
+              System.out.println("You don't have enough chi for this skill.");
+              continue;
+            }
+            myHero.skills[skillNum].cast(myHero);
+            break;
+          } catch (NumberFormatException nfe) {
+            System.out.println("No such command.");
+            continue;
+          }
+        }
+      }
+
+      // Automatically choose skill for boss, if any.
+      myBoss.auto(myHero);
+
+      System.out
+          .println("===================================="
+              + "============================================");
+      // Hero round.
+      // Only attack if no life/chi skill is used.
+      myHero.attack(myBoss);
+      if (myBoss.life <= 0) {
+        roundOver();
+        reward();
+        return true;
+      }
+
+      // Boss round.
+      // Only attack if no life/chi skill is used.
+
+      myBoss.attack(myHero);
+      if (myHero.life <= 0) {
+        System.out.printf("%s was defeated!\n", myHero.name);
+        return false;
+      }
+
+      // round clean up.
+      roundOver();
+    }
+
+    // How could you get here!?
+    return false;
+  }
+
+  /**
+   * Check duration of skill and reset status if expired.
+   */
+  public void roundOver() {
+    if (this.myHero.skill != null) {
+      this.myHero.skill.cancel(this.myHero);
+    }
+    if (this.myBoss.skill != null) {
+      this.myBoss.skill.cancel(this.myBoss);
+    }
+    this.round++;
+    System.out.printf("round %d over\n\n\n\n", this.round);
+  }
+
+  /**
+   *  You win the battle, you get good stuff and become stronger.
+   *  May get stuff from monsters by adding logic here.
+   */
+  void reward() {
+    System.out.printf("%s defeated %s after %d %s !\n", myHero.name, myBoss.name, round,
+        (round > 1) ? "rounds" : "round");
+    this.myHero.grow(this.myBoss);
+  }
 }
--- main/buildinClass/Skill.java
+++ main/buildinClass/Skill.java
@@ -1,106 +1,138 @@
 package buildinClass;
 
 public class Skill {
-	
-		//Effect of this skill.
-		public double lifeMod;
-		public int chiMod;
-		public double attackMod;
-		public double defenseMod;
-		public String name;
-		public int cost;
-		
-		public Skill(String name, double hMod, int chiMod, double aMod, double dMod, int cost) {
-			boolean valid = true;
-			
-			this.name = name;
-			this.lifeMod = hMod;
-			this.chiMod = chiMod;
-			this.attackMod = aMod;
-			this.defenseMod = dMod;
-			this.cost = cost;
-			
-			//Check validity of skill setting.
-			if (attackMod <= -1) {
-				System.err.printf("Invalid attack modifier.\n");
-				valid = false;
-			}
-			if (cost < 0) {
-				System.err.printf("Invalid cost.\n");
-				valid = false;
-			}
-			if (!valid) {
-				System.err.println(name + " is not valid.");
-				System.exit(1);
-			}
-		}
-		
-		//Use this skill.
-		public void cast(Unit character) {
-			System.out.printf("ACTION:\t%s Used skill %s : ", character.name, this.name);
-			if (lifeMod > 0) {
-				double lifePlus = Math.min(lifeMod, character.lifeMax - character.life);
-				System.out.printf("%s restored %.2f health for %s.", this.name, lifePlus, character.name);
-				character.life += lifePlus;
-			}
-			
-			if (chiMod > 0) {
-				int chiPlus = Math.min(chiMod, character.chiMax - character.chi);
-				System.out.printf("%s restored %d chi for %s. ", this.name, chiPlus, character.name);
-				character.chi += chiPlus;
-			}
-			
-			if (attackMod != 0 && this != character.skill) {
-				if (attackMod > 0)
-					System.out.print("Attack increased temporarily by " + attackMod * 100 + "% for " + character.name + ". ");
-				else
-					System.out.print("Attack decreased temporarily by " + attackMod * -100 + "% for " + character.name + ". ");
-				character.attack *= (1 + attackMod);
-			}
-			
-			if (defenseMod != 0 && this != character.skill) {
-				if (defenseMod > 0)
-					System.out.print("Defense increased temporarily by " + defenseMod * 100 + "% for " + character.name + ".");
-				else
-					System.out.print("Defense decreased temporarily by " + defenseMod * 100 + "% for " + character.name + ".");
-				character.defense *= (1 + defenseMod);
-			}
-			character.skill = this;
-			character.chi -= this.cost;
-			System.out.println("");
-			return;
-		}
-		
-		//Cancel skill effect.
-		public void cancel(Unit character) {
-			if (attackMod != 0) {
-				character.attack = character.defaultAttack;
-			}
-			
-			if (defenseMod != 0) {
-				character.defense = character.defaultDefense;
-			}
-			character.skill = null;
-			return;
-		}
-		
-		//Description of the skill.
-		public String effect() {
-			String lifeEffect = (this.lifeMod > 0) ? "Restore " + lifeMod + " life for caster. " :"";
-			String chiEffect = (this.chiMod > 0) ? "Restore " + chiMod + " chi for caster. " :"";
-			String attackEffect = "";
-			String defenseEffect = "";
-			if (this.attackMod != 0)
-				attackEffect = ((this.attackMod > 0) ? "Increase": "Decrease") + " attack by " + attackMod * 100
-					+ "% for this round. ";
-			if (this.defenseMod != 0)
-				defenseEffect = ((this.defenseMod > 0) ? "Increase": "Decrease") + " defense by " + defenseMod * 100
-				+ "% for this round. ";
-			
-			String costEffect = (this.cost > 0) ? "Costs " + cost + " chi. " :"";
-			String totalEffect = lifeEffect + chiEffect + attackEffect + defenseEffect + costEffect;
-			
-			return totalEffect;
-		}
+
+  // Effect of this skill.
+  public double lifeMod;
+  public int chiMod;
+  public double attackMod;
+  public double defenseMod;
+  public String name;
+  public int cost;
+
+  /**
+   * @param name name of the skill.
+   * @param hmod effect of skill on life.
+   * @param chimod effect of skill on chi.
+   * @param amod effect of skill on attack.
+   * @param dmod effect of skill on defense.
+   * @param cost cost of skill.
+   */
+  public Skill(String name, double hmod, int chimod, double amod, double dmod, int cost) {
+    boolean valid = true;
+
+    this.name = name;
+    this.lifeMod = hmod;
+    this.chiMod = chimod;
+    this.attackMod = amod;
+    this.defenseMod = dmod;
+    this.cost = cost;
+
+    // Check validity of skill setting.
+    if (attackMod <= -1) {
+      System.err.printf("Invalid attack modifier.\n");
+      valid = false;
+    }
+    if (cost < 0) {
+      System.err.printf("Invalid cost.\n");
+      valid = false;
+    }
+    if (!valid) {
+      System.err.println(name + " is not valid.");
+      System.exit(1);
+    }
+  }
+
+  /**
+   * Use this skill.
+   * @param character User of the skill.
+   */
+  public void cast(Unit character) {
+    System.out.printf("ACTION:\t%s Used skill %s : ", character.name, this.name);
+    
+    if (lifeMod > 0) {
+      double lifePlus = Math.min(lifeMod, character.lifeMax - character.life);
+      
+      System.out.printf("%s restored %.2f health for %s.", this.name, lifePlus, character.name);
+      character.life += lifePlus;
+    }
+
+    if (chiMod > 0) {
+      int chiPlus = Math.min(chiMod, character.chiMax - character.chi);
+      
+      System.out.printf("%s restored %d chi for %s. ", this.name, chiPlus, character.name);
+      character.chi += chiPlus;
+    }
+
+    if (attackMod != 0 && this != character.skill) {
+      if (attackMod > 0) {
+        System.out.print("Attack increased temporarily by " + attackMod * 100 + "% for "
+            + character.name + ". ");
+      } else {
+        System.out.print("Attack decreased temporarily by " + attackMod * -100 + "% for "
+            + character.name + ". ");
+      }
+      character.attack *= (1 + attackMod);
+    }
+
+    if (defenseMod != 0 && this != character.skill) {
+      if (defenseMod > 0) {
+        System.out.print("Defense increased temporarily by " + defenseMod * 100 + "% for "
+            + character.name + ".");
+      } else {
+        System.out.print("Defense decreased temporarily by " + defenseMod * 100 + "% for "
+            + character.name + ".");
+      }
+      character.defense *= (1 + defenseMod);
+    }
+    
+    character.skill = this;
+    character.chi -= this.cost;
+    System.out.println("");
+    return;
+  }
+
+  /**
+   * Cancel skill effect.
+   * @param character Cancel this skill on character.
+   */
+  public void cancel(Unit character) {
+    if (attackMod != 0) {
+      character.attack = character.defaultAttack;
+    }
+
+    if (defenseMod != 0) {
+      character.defense = character.defaultDefense;
+    }
+    character.skill = null;
+    return;
+  }
+
+  /**
+   * Description of the skill.
+   * @return description string of this skill.
+   */
+  public String effect() {
+    String lifeEffect = (this.lifeMod > 0) ? "Restore " + lifeMod + " life for caster. " : "";
+    String chiEffect = (this.chiMod > 0) ? "Restore " + chiMod + " chi for caster. " : "";
+    String attackEffect = "";
+    String defenseEffect = "";
+    
+    if (this.attackMod != 0) {
+      attackEffect =
+          ((this.attackMod > 0) ? "Increase" : "Decrease") + " attack by " + attackMod * 100
+              + "% for this round. ";
+    }
+    if (this.defenseMod != 0) {
+      defenseEffect =
+          ((this.defenseMod > 0) ? "Increase" : "Decrease") + " defense by " + defenseMod * 100
+              + "% for this round. ";
+    }
+
+    String costEffect = (this.cost > 0) ? "Costs " + cost + " chi. " : "";
+    String totalEffect = lifeEffect + chiEffect + attackEffect + defenseEffect + costEffect;
+
+    return totalEffect;
+  }
 
 }
